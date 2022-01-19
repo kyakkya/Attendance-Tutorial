e8ff42bf28a8365aa9a9d1dc299934e52f090e2d
kyakkya <inataaiko123@gmail.com>
2022-01-19 12:24:57 +0000

    def one_month設定

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -19,4 +19,19 @@ class AttendancesController < ApplicationController
     end  
     redirect_to @user
   end
+  
+  def edit_one_month
+    @first_day = params[:date].nil??
+    Date.current.beginning_of_month: params[:date].to_date
+    @last_day = @first_day.end_of_month
+    one_month = [*@first_day..@last_day]
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+
+  unless one_month.count == @attendances.count # それぞれの件数（日数）が一致するか評価します。
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      # 繰り返し処理により、1ヶ月分の勤怠データを生成します。
+      one_month.each { |day| @user.attendances.create!(worked_on: day) }
+    end
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:wo
+  end  
 end
